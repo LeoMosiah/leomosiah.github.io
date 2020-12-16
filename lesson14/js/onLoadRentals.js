ed3d4c6bb8c2d2416d27c96fc325d62a8b761e50
LeoMosiah <leonardomosiah@gmail.com>
2020-12-16 17:59:16 -0300

    finishing reservation and rentals page

--- lesson14/js/onLoadRentals.js
+++ lesson14/js/onLoadRentals.js
@@ -1,5 +1,29 @@
 function onLoadHome() {
     fetch('./data/rentals.json')
-        .then(response => response.json()).then(res => console.log(res))
+        .then(response => response.json()).then(onRentals)
 }
+
+function onRentals({ options }) {
+    const container = document.getElementById('rentals')
+    container.innerHTML = options.map(option => `
+        <div>
+            <div>
+                <div>
+                    <h2>${option.rentalType}</h2>
+                </div>
+                <img src="./images/hero-image-small.jpg" alt="Image">
+            </div>
+            <div>
+                <div>
+                    <p style="color: white">Max Passengers: ${option.maxPersons}</p>
+                    <p style="color: white">Reservations Prices: </p>
+                    <p style="color: white">Half Day: $ ${option.reservation.half} | Full Day: $ ${option.reservation.full}</p>
+                    <p style="color: white">Walk In Prices: </p>
+                    <p style="color: white">Half Day: $ ${option.walkIn.half} | Full Day: $ ${option.walkIn.full}</p>
+                </div>
+                <a href="reservations.html">Reserve Now</a>
+            </div>
+        </div>`).join("")
+}
+
 onLoadHome()
